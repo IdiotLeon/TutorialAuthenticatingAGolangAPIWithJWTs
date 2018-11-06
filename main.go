80ff2a58ca43f3180ad32c54c17b15e44a3644b6
Yang Lu (Leon) <yanglyu.leon.7@gmail.com>
2018-11-05 20:36:06 -0800

    demo: Using Router Middleware for Cleanliness

--- main.go
+++ main.go
@@ -57,11 +57,48 @@ func ProtectedEndpoint(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func ValidateMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		authorizationHeader := req.Header.Get("authorization")
+		if authorizationHeader != "" {
+			bearerToken := strings.Split(authorizationHeader, " ")
+			if len(bearerToken) == 2 {
+				token, error := jwt.Parse(bearerToken[1], func(token *jwt.Token) (interface{}, error) {
+					if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+						return nil, fmt.Errorf("There was an error")
+					}
+					return []byte("secret"), nil
+				})
+				if error != nil {
+					json.NewEncoder(w).Encode(Exception{Message: error.Error()})
+					return
+				}
+				if token.Valid {
+					context.Set(req, "decoded", token.Claims)
+					next(w, req)
+				} else {
+					json.NewEncoder(w).Encode(Exception{Message: "Invalid authorization token"})
+				}
+			}
+		} else {
+			json.NewEncoder(w).Encode(Exception{Message: "An authorization header is requried"})
+		}
+	})
+}
+
+func TestEndpoint(w http.ResponseWriter, req *http.Request) {
+	decoded := context.Get(req, "decoded")
+	var user User
+	mapstructure.Decode(decoded.(jwt.MapClaims), &user)
+	json.NewEncoder(w).Encode(user)
+}
+
 func main() {
 	router := mux.NewRouter()
 
 	fmt.Println("Starting the apllication...")
 	router.HandleFunc("/authenticate", CreateTokenEndpoint).Methods("POST")
 	router.HandleFunc("/protected", ProtectedEndpoint).Methods("GET")
+	router.HandleFunc("/test", ValidateMiddleware(TestEndpoint)).Methods("GET")
 	log.Fatal(http.ListenAndServe(":12345", router))
 }
